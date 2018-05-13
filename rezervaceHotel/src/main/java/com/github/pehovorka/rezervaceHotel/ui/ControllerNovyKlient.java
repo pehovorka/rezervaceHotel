a5ce448f8d377971a1465ef590931782d121499b
Petr Hovorka <pehovorka@gmail.com>
2018-05-13 11:41:47 +0200

    Kontrola jmen proti existujícím občanským průkazům

--- rezervaceHotel/src/main/java/com/github/pehovorka/rezervaceHotel/ui/ControllerNovyKlient.java
+++ rezervaceHotel/src/main/java/com/github/pehovorka/rezervaceHotel/ui/ControllerNovyKlient.java
@@ -1,5 +1,7 @@
 package com.github.pehovorka.rezervaceHotel.ui;
 
+import java.util.Map.Entry;
+
 import com.github.pehovorka.rezervaceHotel.logika.Klient;
 import com.github.pehovorka.rezervaceHotel.logika.Rezervace;
 
@@ -54,12 +56,30 @@ public class ControllerNovyKlient extends GridPane{
 		}
 		else {
 		try {
+//		if (rezervace.getKlienti().containsKey(Integer.parseInt(cisloOP.getText())) && rezervace.getKlienti().) {
+//			Alert alert = new Alert(AlertType.ERROR);
+//			alert.setTitle("Tento OP je již v databázi");
+//			alert.setHeaderText("Toto číslo občanského průkazu již zadal někdo kdo se jmenuje jinak než vy!");
+//			alert.showAndWait();
+//		}
+//		else {
 		Klient vkladany = new Klient(jmeno.getText(),prijmeni.getText(),Integer.parseInt(cisloOP.getText()));
-		System.out.println("Vkládám: Jméno: "+vkladany.getJmeno()+" Příjmení: "+vkladany.getPrijmeni()+"  "+vkladany.getCisloOP());
+		if (rezervace.getKlienti().containsKey(vkladany.getCisloOP()) && (!rezervace.getKlienti().get(vkladany.getCisloOP()).getJmeno().equals(vkladany.getJmeno()) || !rezervace.getKlienti().get(vkladany.getCisloOP()).getPrijmeni().equals(vkladany.getPrijmeni()))) {	
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setTitle("Tento OP je již v databázi");
+				alert.setHeaderText("Toto číslo občanského průkazu již zadal někdo kdo se jmenuje jinak než vy!");
+				alert.showAndWait();
+		}
+		else {
+		System.out.println("Vkládám: Jméno: "+vkladany.getJmeno()+" Příjmení: "+vkladany.getPrijmeni()+" 'Číslo OP: "+vkladany.getCisloOP());
 		rezervace.vlozKlienta(vkladany);
 		Stage stage = (Stage) buttonPotvrdit.getScene().getWindow();
 	    stage.close();
-	    System.out.println(rezervace.getKlienti().keySet());
+	    for(Entry<Integer, Klient> entry : rezervace.getKlienti().entrySet())
+	    {  
+	    	System.out.println(entry.getKey() + " : " +entry.getValue().getJmeno() + " " + entry.getValue().getPrijmeni());
+	    }
+		}
 		}
 		catch (NumberFormatException e) {
 			Alert alert = new Alert(AlertType.ERROR);
